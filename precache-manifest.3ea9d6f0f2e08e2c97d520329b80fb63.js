a24563b4ebce01f9d65c83d4e6e84fbe8c64f6a0
VenuGopal1899 <venumalladi18@gmail.com>
2020-08-08 10:28:33 +0530

    Updates

---
+++ precache-manifest.3ea9d6f0f2e08e2c97d520329b80fb63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4a1c4329c5a9e64df2580f43e0b02f",
+    "url": "/climate-info/index.html"
+  },
+  {
+    "revision": "fb990dbf564e413301b2",
+    "url": "/climate-info/static/css/2.34ca12c2.chunk.css"
+  },
+  {
+    "revision": "8d23864aa4e20aeb7723",
+    "url": "/climate-info/static/css/main.92cf0d10.chunk.css"
+  },
+  {
+    "revision": "fb990dbf564e413301b2",
+    "url": "/climate-info/static/js/2.d673c65c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/climate-info/static/js/2.d673c65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d23864aa4e20aeb7723",
+    "url": "/climate-info/static/js/main.916c8ec6.chunk.js"
+  },
+  {
+    "revision": "e20b99e9eac5334ecd79",
+    "url": "/climate-info/static/js/runtime-main.a1a6fb86.js"
+  }
+]);
